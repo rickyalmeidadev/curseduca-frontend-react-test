fca6b28f465bc07aecf1d3124bea7fe55454ced5
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-07-09 03:20:20 -0300

    feat: add PostForm component initial markup

---
+++ frontend/src/components/PostForm/indext.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Input, Textarea, Select } from '../';
+
+const PostForm: React.FC = () => {
+  return (
+    <form>
+      <Input type="text" name="title" label="Título" />
+      <Select />
+      <Textarea name="body" label="Corpo" />
+    </form>
+  );
+};
+
+export default PostForm;
