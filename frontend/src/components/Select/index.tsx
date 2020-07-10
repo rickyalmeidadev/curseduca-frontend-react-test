e697a95d0b9fc376abd66f2f40502b708592608c
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-07-10 14:18:15 -0300

    feat: add styles to Select component

--- frontend/src/components/Select/index.tsx
+++ frontend/src/components/Select/index.tsx
@@ -1,13 +1,18 @@
 import React, { SelectHTMLAttributes } from 'react';
 
+import { StyledSelect, Label } from './styles';
+
 const Select: React.FC<SelectHTMLAttributes<HTMLSelectElement>> = ({ ...rest }) => {
   return (
-    <select {...rest} name="categoryId" id="categoryId">
-      <option value={1}>Artigos</option>
-      <option value={2}>Pensamentos</option>
-      <option value={3}>Avisos</option>
-      <option value={4}>Conteúdos</option>
-    </select>
+    <Label>
+      Categoria
+      <StyledSelect {...rest} name="categoryId" id="categoryId">
+        <option value={1}>Artigos</option>
+        <option value={2}>Pensamentos</option>
+        <option value={3}>Avisos</option>
+        <option value={4}>Conteúdos</option>
+      </StyledSelect>
+    </Label>
   );
 };
 
---
+++ frontend/src/components/Select/styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  margin-bottom: ${props => props.theme.margins.normal};
+
+  font-weight: bold;
+  font-size: 2rem;
+  font-variant: small-caps;
+
+  letter-spacing: 0.02em;
+
+  color: ${props => props.theme.colors.dark};
+`;
+
+export const StyledSelect = styled.select`
+  width: 100%;
+
+  margin-top: ${props => props.theme.margins.normal};
+  padding: ${props => props.theme.paddings.normal};
+
+  box-shadow: 18px 18px 30px #d1d9e6, inset 10px 10px 30px rgba(255, 255, 255, 0.4);
+  background-color: ${props => props.theme.colors.light};
+
+  border: none;
+  border-radius: ${props => props.theme.borderRadius};
+
+  font-weight: normal;
+  font-size: 1.8rem;
+  letter-spacing: 0.02em;
+
+  color: ${props => props.theme.colors.dark};
+
+  ::placeholder {
+    color: ${props => props.theme.colors.grey};
+  }
+
+  :focus {
+    border: 2px solid #a6bba8;
+  }
+
+  @media (min-width: 768px) {
+    border: 2px solid ${props => props.theme.colors.light};
+  }
+
+  > option {
+    border: none;
+    background-color: ${props => props.theme.colors.light};
+  }
+`;
