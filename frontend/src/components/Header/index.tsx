718013a78632079c57683eb7f7fc7b3d4a6d111b
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-07-11 00:21:03 -0300

    fix: make props optional for Header component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,18 +9,18 @@ import logo from '../../assets/green-logo.png';
 import { FiMenu } from 'react-icons/fi';
 
 interface Props {
-  toggle: boolean;
-  handleToggle(): void;
+  toggle?: boolean;
+  handleToggle?(): void;
 }
 
-const Header: React.FC<Props> = ({ toggle, handleToggle }) => {
+const Header: React.FC<Props> = ({ toggle = null, handleToggle }) => {
   return (
     <Container>
       <Content>
         <Logo>
           <img src={logo} alt="CursEduca blog" />
         </Logo>
-        <FiMenu size="28px" color="#2A2A2A" onClick={handleToggle} />
+        {toggle !== null ? <FiMenu size="28px" color="#2A2A2A" onClick={handleToggle} /> : null}
         <Button primary>Adicionar Post</Button>
       </Content>
     </Container>
